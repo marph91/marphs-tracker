c47487c288616f47ddc961efb658e1f1a0c11d46
Martin Dörfelt <33229141+marph91@users.noreply.github.com>
2026-09-17 18:32:05 +0200

    put sleep preparation to cycle

--- firmware/main.py
+++ firmware/main.py
@@ -15,68 +15,6 @@ from tracker.wifi_scan import scan_wifi
 LOG = logger.Logger(__name__.strip("_"))
 
 
-def prepare_for_sleep(pmu, modem):
-    # based on:
-    # - https://github.com/Xinyuan-LilyGO/LilyGo-T-SIM7080G-MicroPython/blob/377b15a71fde63463ef67a450856571dc5516a8a/examples/MinimalModemAndEspSleep/MinimalModemAndEspSleep.py
-    # - https://github.com/Xinyuan-LilyGO/LilyGo-T-SIM7080G/issues/168
-
-    modem.power_off()
-    pmu.power_down_for_sleep()
-
-    # TODO: needed?
-    # -------------------------------------------------------------
-    # Put unused GPIOs into low-power states
-    # -------------------------------------------------------------
-    # def input_pin(pin):
-    #     machine.Pin(pin, machine.Pin.IN)
-
-    # def pulldown_pin(pin):
-    #     machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_DOWN)
-
-    # # I2C
-    # input_pin(utilities.I2C_SDA)
-    # input_pin(utilities.I2C_SCL)
-
-    # # SD card
-    # input_pin(38)  # SDMMC_CLK
-    # input_pin(39)  # SDMMC_CMD
-    # input_pin(40)  # SDMMC_DATA
-
-    # # LEDs / miscellaneous inputs
-    # pulldown_pin(8)
-    # pulldown_pin(9)
-    # pulldown_pin(10)
-
-    # # User/application inputs
-    # pulldown_pin(11)
-    # pulldown_pin(12)
-
-    # # PWM
-    # pulldown_pin(14)
-
-    # # PN532 reset
-    # pulldown_pin(16)
-
-    # # Camera / unused pins
-    # pulldown_pin(1)
-    # pulldown_pin(2)
-
-    # pulldown_pin(21)
-    # pulldown_pin(45)
-    # pulldown_pin(46)
-    # pulldown_pin(47)
-    # pulldown_pin(48)
-
-    # -------------------------------------------------------------
-    # UARTs
-    # -------------------------------------------------------------
-    # Stop modem UART
-    modem.uart.deinit()
-
-    # UART2/NFC is not used here.
-    # uart2.deinit()
-
-
 def main():
     LOG("Starting main script")
 
@@ -101,23 +39,21 @@ def main():
     elapsed_time_ms = time.ticks_diff(time.ticks_ms(), start_time_ms)
     LOG(f"Cycle time: {elapsed_time_ms / 1000} s")
 
-    try:
-        prepare_for_sleep(pmu, modem)
-    finally:
-        if pmu.get_battery_percent() == -1:
-            # prevent boot loop to allow debugging
-            # stop the script with ctrl+c
-            LOG("Don't go to sleep, since the battery is not connected.")
-        else:
-            # sleep some time depending on the state
-            sleep_minutes = (
-                config.SLEEP_MINUTES_HOME
-                if cycle_state == CycleState.HOME
-                else config.SLEEP_MINUTES_AWAY
-            )
-            sleep_ms = int(sleep_minutes) * 60 * 1000
-            LOG(f"Going to sleep for {sleep_ms / 1000} seconds")
-            machine.deepsleep(sleep_ms)
+    # sleep
+    if pmu.get_battery_percent() == -1:
+        # prevent boot loop to allow debugging
+        # stop the script with ctrl+c
+        LOG("Don't go to sleep, since the battery is not connected.")
+    else:
+        # sleep some time depending on the state
+        sleep_minutes = (
+            config.SLEEP_MINUTES_HOME
+            if cycle_state == CycleState.HOME
+            else config.SLEEP_MINUTES_AWAY
+        )
+        sleep_ms = int(sleep_minutes) * 60 * 1000
+        LOG(f"Going to sleep for {sleep_ms / 1000} seconds")
+        machine.deepsleep(sleep_ms)
 
 
 if __name__ == "__main__":
--- firmware/tracker/cycle.py
+++ firmware/tracker/cycle.py
@@ -18,7 +18,70 @@ class CycleState:
     NO_FIX = "NO_FIX"
     POST_FAILED = "POST_FAILED"
     CONFIG_ERROR = "CONFIG_ERROR"
+    SLEEP_PREPARATION_FAILED = "SLEEP_PREPARATION_FAILED"
     FINISHED = "FINISHED"
+
+
+def prepare_for_sleep(pmu, modem):
+    # based on:
+    # - https://github.com/Xinyuan-LilyGO/LilyGo-T-SIM7080G-MicroPython/blob/377b15a71fde63463ef67a450856571dc5516a8a/examples/MinimalModemAndEspSleep/MinimalModemAndEspSleep.py
+    # - https://github.com/Xinyuan-LilyGO/LilyGo-T-SIM7080G/issues/168
+
+    modem.power_off()
+    pmu.power_down_for_sleep()
+
+    # TODO: needed?
+    # -------------------------------------------------------------
+    # Put unused GPIOs into low-power states
+    # -------------------------------------------------------------
+    # def input_pin(pin):
+    #     machine.Pin(pin, machine.Pin.IN)
+
+    # def pulldown_pin(pin):
+    #     machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
+    # # I2C
+    # input_pin(utilities.I2C_SDA)
+    # input_pin(utilities.I2C_SCL)
+
+    # # SD card
+    # input_pin(38)  # SDMMC_CLK
+    # input_pin(39)  # SDMMC_CMD
+    # input_pin(40)  # SDMMC_DATA
+
+    # # LEDs / miscellaneous inputs
+    # pulldown_pin(8)
+    # pulldown_pin(9)
+    # pulldown_pin(10)
+
+    # # User/application inputs
+    # pulldown_pin(11)
+    # pulldown_pin(12)
+
+    # # PWM
+    # pulldown_pin(14)
+
+    # # PN532 reset
+    # pulldown_pin(16)
+
+    # # Camera / unused pins
+    # pulldown_pin(1)
+    # pulldown_pin(2)
+
+    # pulldown_pin(21)
+    # pulldown_pin(45)
+    # pulldown_pin(46)
+    # pulldown_pin(47)
+    # pulldown_pin(48)
+
+    # -------------------------------------------------------------
+    # UARTs
+    # -------------------------------------------------------------
+    # Stop modem UART
+    modem.uart.deinit()
+
+    # UART2/NFC is not used here.
+    # uart2.deinit()
 
 
 def run_cycle(config, hw_functions):
@@ -87,5 +150,11 @@ def run_cycle(config, hw_functions):
         return CycleState.POST_FAILED if payload else CycleState.CONFIG_ERROR
     finally:
         hw_functions["cellular_data"].disconnect()
+
+    try:
+        prepare_for_sleep(hw_functions["pmu"], hw_functions["modem"])
+    except Exception as exc:  # noqa: BLE001  # want to catch all exceptions
+        LOG(f"{exc}")
+        return CycleState.SLEEP_PREPARATION_FAILED
 
     return CycleState.FINISHED
